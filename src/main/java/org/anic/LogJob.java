b32426cd5d440a63ac0d56a77ffc225a332e1438
Markus Blunier <mblunier@gmx.ch>
2017-03-21 07:38:04 +0100

    Simulate some work by delaying

--- src/main/java/org/anic/LogJob.java
+++ src/main/java/org/anic/LogJob.java
@@ -6,12 +6,13 @@
 package org.anic;
 
 import java.util.Date;
-import org.quartz.Job;
-import org.quartz.JobDetail;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import java.util.Random;
+
+import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static java.lang.Math.abs;
 
 /**
  *
@@ -20,22 +21,27 @@ import org.slf4j.LoggerFactory;
 public class LogJob implements Job {
     
     static Logger log = LoggerFactory.getLogger(LogJob.class);
+    static Random random = new Random();
     
-    @Override
     public void execute (JobExecutionContext context) throws JobExecutionException {
         //Job job = context.getJobInstance();
         JobDetail detail = context.getJobDetail();
+        Trigger trigger = context.getTrigger();
         Date now = new Date();
         Date fireTime = context.getFireTime();
         Date nextTime = context.getNextFireTime();
-        Date schedTime = context.getScheduledFireTime();
-        
-        log.info("{}: now={}, schedTime={}, fireTime={}, nextTime={}",
-                 detail.getKey(), now, schedTime, fireTime, nextTime);
-//        System.out.println(detail.getKey() +
-//                ": now=" + now +
-//                ", schedTime=" + schedTime +
-//                ", fireTime=" + fireTime +
-//                ", nextTime=" + nextTime);
+        Date scheduledTime = context.getScheduledFireTime();
+
+        long delay = abs(10 + random.nextLong() % 60000);
+
+        log.info("{}: trigger={}, now={}, scheduledTime={}, fireTime={}, nextTime={}, delay={}",
+                 detail.getKey(), trigger.getKey(), now, scheduledTime, fireTime, nextTime, delay);
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            log.info("{}: INTERRUPTED", detail.getKey());
+        } finally {
+            log.info("{}: done", detail.getKey());
+        }
     }
 }
